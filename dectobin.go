d377917b5c73766ef252c4ee50bd9ff90d02927b
Peter Oto <pduke360@gmail.com>
2020-04-11 23:28:01 +0100

    first commit

---
+++ dectobin.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var n, input int
+
+func main() {
+	fmt.Scanln(&input)
+
+	n = 1
+
+	for n*2 <= input {
+		n = n * 2
+	}
+
+	for n >= 1 {
+		fmt.Print(input / n)
+		input = input % n
+		n = n / 2
+	}
+}
